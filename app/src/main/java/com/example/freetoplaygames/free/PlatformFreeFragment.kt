0713df08e0fa35a06b858f67560fe4c2019ff921
Alex0656 <waywwwwalex@gmail.com>
2022-06-14 04:59:55 +0700

    Создал 2 экрана, которые можно открыть, нажимая на боттом нав бар

--- app/src/main/java/com/example/freetoplaygames/free/PlatformFreeFragment.kt
+++ app/src/main/java/com/example/freetoplaygames/free/PlatformFreeFragment.kt
@@ -1,13 +1,18 @@
 package com.example.freetoplaygames.free
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.commit
 import com.example.freetoplaygames.R
 import com.example.freetoplaygames.databinding.FragmentPlatformFreeBinding
+import com.example.freetoplaygames.discount.PlatformDiscountFragment
 
 class PlatformFreeFragment : Fragment() {
 
@@ -23,14 +28,33 @@ class PlatformFreeFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.Steam.setOnClickListener {
+
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+            transaction?.replace(R.id.Steam, FreeSteamFragment())
+            transaction?.disallowAddToBackStack()
+            transaction?.commit()
+            /*
+            var fragment = FreeSteamFragment()
+            //childFragmentManager.beginTransaction().replace(R.id.Steam,ChildFragment.getInstance()).commit()
+            childFragmentManager.beginTransaction().replace(R.id.Steam,fragment).commit()
+            */
+        }
+
+
         /*
-        binding.imgGood1.setOnClickListener {
-            val intent = Intent(context, CourseDetailsActivity::class.java)
-            startActivity(intent)
+        binding.Steam.setOnClickListener {
+
+
+            //val intent = Intent(context, CourseDetailsActivity::class.java)
+            //startActivity(intent)
         }
         */
+
+
     }
 }
